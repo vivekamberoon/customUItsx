d2d095e09a6501267d7030067c76638b9850b5c7
vivekamberoon <vivek.singh@amberoon.com>
2020-12-31 18:28:53 +0530

    added component

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import component from './component';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         >
           Learn React
         </a>
+        <component></component>
       </header>
     </div>
   );
